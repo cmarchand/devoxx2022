188b6c8d00c046214d67bd54e5a05341a44712af
Christophe Marchand <cmarchand@oxiane.com>
2022-04-09 17:33:52 +0200

    Ajout de l'entrée de l'enum UNKOWN

--- src/main/java/com/oxiane/formation/devoxx22/refacto/helpers/impl/PrixUnitCalculateurImpl.java
+++ src/main/java/com/oxiane/formation/devoxx22/refacto/helpers/impl/PrixUnitCalculateurImpl.java
@@ -99,6 +99,14 @@ public class PrixUnitCalculateurImpl implements PrixUnitCalculateur {
                         new Seuil(20, BigDecimal.valueOf(0.17)),
                         new Seuil(15, BigDecimal.valueOf(0.1))
                 )
+        ),
+        UNKOWN(
+                "",
+                Arrays.asList(
+                        new Seuil(50, BigDecimal.valueOf(0.2)),
+                        new Seuil(20, BigDecimal.valueOf(0.15)),
+                        new Seuil(10, BigDecimal.valueOf(0.1))
+                )
         )
         ;
         record Seuil(int qte, BigDecimal remise) {};
